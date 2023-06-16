636268fdf9738b53e6f249d5570169b2084e7d26
Alex Jercan <jercan_alex27@yahoo.com>
2023-06-16 16:39:07 +0300

    chore(prettier): ww

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
 #!/usr/bin/env node
 
-import yargs from 'yargs/yargs';
-import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs/yargs'
+import { hideBin } from 'yargs/helpers'
 
-import { run, init } from "./src/jdsl.js";
+import { run, init } from './src/jdsl.js'
 
 yargs(hideBin(process.argv))
-    .command('init <remote>', 'initialize the project', (yargs) => {
-        return yargs
-            .positional('remote', {
+    .command(
+        'init <remote>',
+        'initialize the project',
+        (yargs) => {
+            return yargs.positional('remote', {
                 describe: 'the URL to the remote repository',
-                type: 'string'
+                type: 'string',
             })
-    }, (argv) => {
-        init(argv.remote);
-    })
-    .command('run <className> <functionName>', 'run the project', (yargs) => {
-        return yargs
-            .positional('className', {
-                describe: 'the name of the entrypoint class',
-                type: 'string'
-            })
-            .positional('functionName', {
-                describe: 'the name of the entrypoint function',
-                type: 'string'
-            })
-    }, (argv) => {
-        run(argv.className, argv.functionName);
-    })
-    .parse();
+        },
+        (argv) => {
+            init(argv.remote)
+        }
+    )
+    .command(
+        'run <className> <functionName>',
+        'run the project',
+        (yargs) => {
+            return yargs
+                .positional('className', {
+                    describe: 'the name of the entrypoint class',
+                    type: 'string',
+                })
+                .positional('functionName', {
+                    describe: 'the name of the entrypoint function',
+                    type: 'string',
+                })
+        },
+        (argv) => {
+            run(argv.className, argv.functionName)
+        }
+    )
+    .parse()
